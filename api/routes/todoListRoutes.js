639cba2289975988dfc7ceac7ec6f62a45a124bd
Yaroslav <kiritok712@gmail.com>
2024-08-12 12:53:44 +0200

    Add files via upload

---
+++ api/routes/todoListRoutes.js
@@ -0,0 +1,177 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Task:
+ *       type: object
+ *       required:
+ *         - title
+ *         - author
+ *         - finished
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the task
+ *         title:
+ *           type: string
+ *           description: The title of your task
+ *         author:
+ *           type: string
+ *           description: The task author
+ *         finished:
+ *           type: boolean
+ *           description: Whether you have finished the task
+ *         createdAt:
+ *           type: string
+ *           format: date
+ *           description: The date the task was added
+ *       example:
+ *         id: d5fE_asz
+ *         title: Complete Assignment
+ *         author: John Doe
+ *         finished: false
+ *         createdAt: 2020-03-10T04:05:06.157Z
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Tasks
+ *   description: The tasks managing API
+ * /tasks:
+ *   post:
+ *     summary: Create a new task
+ *     tags: [Tasks]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Task'
+ *     responses:
+ *       200:
+ *         description: The created task.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Task'
+ *       500:
+ *         description: Some server error
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Tasks
+ *   description: The tasks managing API
+ * /tasks:
+ *   get:
+ *     summary: Lists all the tasks
+ *     tags: [Tasks]
+ *     responses:
+ *       200:
+ *         description: The list of the tasks
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Task'
+ *   post:
+ *     summary: Create a new task
+ *     tags: [Tasks]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Task'
+ *     responses:
+ *       200:
+ *         description: The created task.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Task'
+ *       500:
+ *         description: Some server error
+ * /tasks/{id}:
+ *   get:
+ *     summary: Get the task by id
+ *     tags: [Tasks]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The task id
+ *     responses:
+ *       200:
+ *         description: The task response by id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Task'
+ *       404:
+ *         description: The task was not found
+ *   put:
+ *    summary: Update the task by the id
+ *    tags: [Tasks]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The task id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Task'
+ *    responses:
+ *      200:
+ *        description: The task was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Task'
+ *      404:
+ *        description: The task was not found
+ *      500:
+ *        description: Some error happened
+ *   delete:
+ *     summary: Remove the task by id
+ *     tags: [Tasks]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The task id
+ *
+ *     responses:
+ *       200:
+ *         description: The task was deleted
+ *       404:
+ *         description: The task was not found
+ */
+
+'use strict';
+module.exports = function(app) {
+  var todoList = require('../controllers/todoListController');
+
+  // todoList Routes
+  app.route('/tasks')
+    .get(todoList.list_all_tasks)
+    .post(todoList.create_a_task);
+
+
+  app.route('/tasks/:taskId')
+    .get(todoList.read_a_task)
+    .put(todoList.update_a_task)
+    .delete(todoList.delete_a_task);
+};
